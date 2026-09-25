1b45b2d5024170c6307a8aab9cea88451c91e65c
Marcin Polak <marcin@coder.pm>
2026-09-25 05:22:07 +0000

    cleat 1.5.4

--- Formula/cleat.rb
+++ Formula/cleat.rb
@@ -1,8 +1,8 @@
 class Cleat < Formula
   desc "Docker sandbox for AI coding agents"
   homepage "https://cleat.sh"
-  url "https://github.com/cleatdev/cleat/releases/download/v1.5.3/cleat-1.5.3.tar.gz"
-  sha256 "7ae2c91d3ad9253e1f61b4dab6852faf18a5a22af8aa76ee8727b9775ffb954b"
+  url "https://github.com/cleatdev/cleat/releases/download/v1.5.4/cleat-1.5.4.tar.gz"
+  sha256 "273c3dc3c4e31822c96904f8ae85f333f60c7cdf52349c3cfae3a4cedb1a1691"
   license "MIT"
 
   def install
